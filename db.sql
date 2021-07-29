01242737dbe4308ffd4eb7254b5dab9636e7b771
Chana Fine <chanafine7@gmail.com>
2021-07-29 00:36:40 -0400

    quote form and database creation script

---
+++ db.sql
@@ -0,0 +1,46 @@
+CREATE TABLE orders (
+    id VARCHAR(64) NOT NULL,
+    user_id VARCHAR(64) NOT NULL,
+    date_created DATETIME NOT NULL,
+    date_deleted DATETIME NULL,
+    event_date DATETIME NOT NULL,
+    event_type VARCHAR(64) NOT NULL,
+    delivery SMALLINT NOT NULL,
+    delivery_fee DOUBLE NULL,
+    delivery_date DATETIME NOT NULL,
+    total_price DOUBLE NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES USER(id)
+) ENGINE = INNODB;
+
+CREATE TABLE cart(
+    id VARCHAR(64) NOT NULL,
+    user_id VARCHAR(64) NOT NULL,
+    date_created DATETIME NOT NULL,
+    date_deleted DATETIME NULL,
+    total_price DOUBLE NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES USER(id)
+) ENGINE = INNODB;
+
+CREATE TABLE cart_items(
+    cart_id VARCHAR(64) NOT NULL,
+    product_id VARCHAR(64) NOT NULL,
+    item_qty INT NOT NULL,
+    PRIMARY KEY(cart_id, product_id),
+    date_created DATETIME NOT NULL,
+    date_deleted DATETIME NULL,
+    FOREIGN KEY(cart_id) REFERENCES cart(id),
+    FOREIGN KEY(product_id) REFERENCES product(id)
+) ENGINE = INNODB;
+
+CREATE TABLE order_details(
+    order_id VARCHAR(64) NOT NULL,
+    product_id VARCHAR(64) NOT NULL,
+    item_qty INT NOT NULL,
+    PRIMARY KEY(order_id, product_id),
+    date_created DATETIME NOT NULL,
+    date_deleted DATETIME NULL,
+    FOREIGN KEY(order_id) REFERENCES ORDERs(id),
+    FOREIGN KEY(product_id) REFERENCES product(id)
+) ENGINE = INNODB;
